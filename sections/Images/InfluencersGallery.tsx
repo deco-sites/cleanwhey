b2d1576c5efeb1db0206d592fb3786a3debe7cda
Diego <diego.o.guimaraes29@gmail.com>
2024-09-07 13:31:12 -0300

    Fix: find out more

--- sections/Images/InfluencersGallery.tsx
+++ sections/Images/InfluencersGallery.tsx
@@ -41,7 +41,9 @@ const Desktop = ({ items, conteudoFlutuante }: Props) => {
 
                 <div class="font-regular text-sm leading-4 transition-all text-left text-white z-10 py-8 px-6 duration-300 hidden bg-[#385A88CC] group-hover:flex flex-col w-full h-full z-10 absolute top-0 left-0 overflow-y-scroll items-start justify-start scroll-custom overflow-x-hidden rounded-md">
                   <h2 class={"font-bold mb-6"}>{item.textLabel}</h2>
-                  <span dangerouslySetInnerHTML={{ __html: item.description }} />
+                  <span
+                    dangerouslySetInnerHTML={{ __html: item.description }}
+                  />
                 </div>
               </a>
               <p class="z-20 w-[90%] group-hover:hidden absolute top-8 bg-[#163E73D9] p-1.5 font-regular rounded-lg text-white  text-center text-lg ">
@@ -50,7 +52,7 @@ const Desktop = ({ items, conteudoFlutuante }: Props) => {
             </div>
           ))}
       </div>
-      <div class="relative -mt-12 m-auto z-30 !text-center max-w-lg border rounded-lg p-6 border-gray-200 bg-white">
+      <div class="relative mt-2.5 m-auto z-30 !text-center max-w-lg border rounded-lg p-6 border-gray-200 bg-white">
         <div
           class="text-gray-300 mb-4"
           dangerouslySetInnerHTML={{ __html: conteudoFlutuante.text }}
@@ -90,7 +92,9 @@ const Mobile = ({ items, conteudoFlutuante }: Props) => {
 
                   <div class="font-regular text-sm leading-4 transition-all text-left text-white z-10 py-8 px-6 duration-300 hidden bg-[#385A88CC] group-hover:flex flex-col w-full h-full z-10 absolute top-0 left-0 overflow-y-scroll items-start justify-start scroll-custom overflow-x-hidden rounded-md">
                     <h2 class={"font-bold mb-6"}>{item.textLabel}</h2>
-                    <span dangerouslySetInnerHTML={{ __html: item.description }} />
+                    <span
+                      dangerouslySetInnerHTML={{ __html: item.description }}
+                    />
                   </div>
                 </a>
                 <p class="z-20 w-[90%] group-hover:hidden absolute top-8 bg-[#163E73D9] p-1.5 font-regular rounded-lg text-white  text-center text-lg">
@@ -100,22 +104,20 @@ const Mobile = ({ items, conteudoFlutuante }: Props) => {
             ))}
         </Slider>
 
-        <div class="rounded-full border-gray-200 border bg-white w-8 h-8 flex items-center 
-                justify-center absolute top-2/4 left-4">
-          <Slider.PrevButton
-            class="cursor-pointer"
-            disabled={false}
-          >
+        <div
+          class="rounded-full border-gray-200 border bg-white w-8 h-8 flex items-center 
+                justify-center absolute top-2/4 left-4"
+        >
+          <Slider.PrevButton class="cursor-pointer" disabled={false}>
             <Icon id="chevron-right" class="rotate-180 text-orange-300" />
           </Slider.PrevButton>
         </div>
 
-        <div class="rounded-full border-gray-200 border bg-white w-8 h-8 flex items-center 
-                justify-center absolute top-2/4 right-4">
-          <Slider.NextButton
-            class="cursor-pointer"
-            disabled={false}
-          >
+        <div
+          class="rounded-full border-gray-200 border bg-white w-8 h-8 flex items-center 
+                justify-center absolute top-2/4 right-4"
+        >
+          <Slider.NextButton class="cursor-pointer" disabled={false}>
             <Icon id="chevron-right" class="text-orange-300" />
           </Slider.NextButton>
         </div>
@@ -123,7 +125,7 @@ const Mobile = ({ items, conteudoFlutuante }: Props) => {
         <Slider.JS rootId={id} />
       </div>
 
-      <div class="relative -mt-12 m-auto z-30 !text-center max-w-[90%] border rounded-lg p-6 border-gray-200 bg-white">
+      <div class="relative mt-2.5 m-auto z-30 !text-center max-w-[90%] border rounded-lg p-6 border-gray-200 bg-white">
         <div
           class="text-gray-300 mb-4"
           dangerouslySetInnerHTML={{ __html: conteudoFlutuante.text }}
@@ -139,9 +141,11 @@ const Mobile = ({ items, conteudoFlutuante }: Props) => {
   );
 };
 
-export default function InfluencersGallery(
-  { title, items, conteudoFlutuante }: Props,
-) {
+export default function InfluencersGallery({
+  title,
+  items,
+  conteudoFlutuante,
+}: Props) {
   const device = useDevice();
   return (
     <section class="section container mb-12">
@@ -154,9 +158,11 @@ export default function InfluencersGallery(
         </div>
       )}
       <div className={`content`}>
-        {device === "desktop"
-          ? <Desktop items={items} conteudoFlutuante={conteudoFlutuante} />
-          : <Mobile items={items} conteudoFlutuante={conteudoFlutuante} />}
+        {device === "desktop" ? (
+          <Desktop items={items} conteudoFlutuante={conteudoFlutuante} />
+        ) : (
+          <Mobile items={items} conteudoFlutuante={conteudoFlutuante} />
+        )}
       </div>
     </section>
   );
