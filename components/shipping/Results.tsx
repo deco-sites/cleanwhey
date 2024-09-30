6181d547561d489f69b1e0239d1d476808301e9c
Diego <diego.o.guimaraes29@gmail.com>
2024-09-30 15:13:09 -0300

    fix: atualição do deno task

--- components/shipping/Results.tsx
+++ components/shipping/Results.tsx
@@ -5,6 +5,7 @@ import { AppContext } from "apps/vtex/mod.ts";
 import type { SimulationOrderForm, SKU, Sla } from "apps/vtex/utils/types.ts";
 import { formatPrice } from "../../sdk/format.ts";
 import { ComponentProps } from "../../sections/Component.tsx";
+import Icon from "../../components/ui/Icon.tsx";
 
 export interface Props {
   items: SKU[];
@@ -13,8 +14,8 @@ export interface Props {
 const formatShippingEstimate = (estimate: string) => {
   const [, time, type] = estimate.split(/(\d+)/);
 
-  if (type === "bd") return `${time} dias úteis`;
-  if (type === "d") return `${time} dias`;
+  if (type === "bd") return `${time} Dias`;
+  if (type === "d") return `${time} Dias`;
   if (type === "h") return `${time} horas`;
 };
 
@@ -22,12 +23,12 @@ export async function action(props: Props, req: Request, ctx: AppContext) {
   const form = await req.formData();
 
   try {
-    const result = await ctx.invoke("vtex/actions/cart/simulation.ts", {
+    const result = (await ctx.invoke("vtex/actions/cart/simulation.ts", {
       items: props.items,
       postalCode: `${form.get("postalCode") ?? ""}`,
       country: "BRA",
-    }) as SimulationOrderForm | null;
-
+    })) as SimulationOrderForm | null;
+    console.log(result);
     return { result };
   } catch {
     return { result: null };
@@ -35,10 +36,11 @@ export async function action(props: Props, req: Request, ctx: AppContext) {
 }
 
 export default function Results({ result }: ComponentProps<typeof action>) {
-  const methods = result?.logisticsInfo?.reduce(
-    (initial, { slas }) => [...initial, ...slas],
-    [] as Sla[],
-  ) ?? [];
+  const methods =
+    result?.logisticsInfo?.reduce(
+      (initial, { slas }) => [...initial, ...slas],
+      [] as Sla[]
+    ) ?? [];
 
   if (!methods.length) {
     return (
@@ -50,26 +52,23 @@ export default function Results({ result }: ComponentProps<typeof action>) {
 
   return (
     <ul class="flex flex-col gap-4 p-4 border border-base-300 rounded">
+      <span> Opções de frete para o CEP</span> <span> X</span>
+      {result !== null && <span>{result.postalCode}</span> }
       {methods.map((method) => (
-        <li class="flex justify-between items-center border-base-200 not-first-child:border-t">
-          <span class="text-button text-center">
-            Entrega {method.name}
+        <li class="flex justify-between items-center border-base-200 not-first-child:border-t text-[#A1A6B7]">
+          <Icon id="truck" size={24} stroke="0.01" />
+
+          <span class="text-button w-1/4 text-start">{method.name}:</span>
+          <span class="text-button text-[#A1A6B7] w-1/4">
+            {formatShippingEstimate(method.shippingEstimate)}
           </span>
-          <span class="text-button">
-            até {formatShippingEstimate(method.shippingEstimate)}
-          </span>
-          <span class="text-base font-semibold text-right">
-            {method.price === 0 ? "Grátis" : (
-              formatPrice(method.price / 100, "BRL", "pt-BR")
-            )}
+          <span class="text-base font-semibold text-right w-1/4">
+            {method.price === 0
+              ? "Grátis"
+              : formatPrice(method.price / 100, "BRL", "pt-BR")}
           </span>
         </li>
       ))}
-      <span class="text-xs font-normal">
-        Os prazos de entrega começam a contar a partir da confirmação do
-        pagamento e podem variar de acordo com a quantidade de produtos na
-        sacola.
-      </span>
     </ul>
   );
 }
