361a42e699678392fb54cd86900cb245c4668413
Henrique Costa <henrique@nois.agency>
2024-10-07 21:12:54 +0000

    Publish 2 from nois into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -47,19 +47,19 @@ import * as $$$$$$28 from "./sections/Links/LinkTree.tsx";
 import * as $$$$$$29 from "./sections/Miscellaneous/CampaignTimer.tsx";
 import * as $$$$$$30 from "./sections/Miscellaneous/CookieConsent.tsx";
 import * as $$$$$$31 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$39 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$40 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$41 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$42 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$43 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$44 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$32 from "./sections/ProductPage/DescriptionBannerBenefits.tsx";
-import * as $$$$$$33 from "./sections/ProductPage/DescriptionBannerRight.tsx";
-import * as $$$$$$34 from "./sections/ProductPage/DescriptionContent.tsx";
-import * as $$$$$$35 from "./sections/ProductPage/DescriptionFullbanner.tsx";
-import * as $$$$$$36 from "./sections/ProductPage/DescriptionMoreInfos.tsx";
-import * as $$$$$$37 from "./sections/ProductPage/DescriptionNavigator.tsx";
-import * as $$$$$$38 from "./sections/ProductPage/NutritionalInformation.tsx";
+import * as $$$$$$32 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$33 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$34 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$35 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$36 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$37 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$38 from "./sections/ProductPage/DescriptionBannerBenefits.tsx";
+import * as $$$$$$39 from "./sections/ProductPage/DescriptionBannerRight.tsx";
+import * as $$$$$$40 from "./sections/ProductPage/DescriptionContent.tsx";
+import * as $$$$$$41 from "./sections/ProductPage/DescriptionFullbanner.tsx";
+import * as $$$$$$42 from "./sections/ProductPage/DescriptionMoreInfos.tsx";
+import * as $$$$$$43 from "./sections/ProductPage/DescriptionNavigator.tsx";
+import * as $$$$$$44 from "./sections/ProductPage/NutritionalInformation.tsx";
 import * as $$$$$$45 from "./sections/Session.tsx";
 import * as $$$$$$46 from "./sections/Social/InstagramPosts.tsx";
 import * as $$$$$$47 from "./sections/Social/WhatsApp.tsx";
@@ -107,19 +107,19 @@ const manifest = {
     "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$29,
     "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$30,
     "site/sections/Newsletter/Newsletter.tsx": $$$$$$31,
-    "site/sections/Product/ProductDetails.tsx": $$$$$$39,
-    "site/sections/Product/ProductShelf.tsx": $$$$$$40,
-    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$41,
-    "site/sections/Product/SearchResult.tsx": $$$$$$42,
-    "site/sections/Product/ShelfWithImage.tsx": $$$$$$43,
-    "site/sections/Product/Wishlist.tsx": $$$$$$44,
-    "site/sections/ProductPage/DescriptionBannerBenefits.tsx": $$$$$$32,
-    "site/sections/ProductPage/DescriptionBannerRight.tsx": $$$$$$33,
-    "site/sections/ProductPage/DescriptionContent.tsx": $$$$$$34,
-    "site/sections/ProductPage/DescriptionFullbanner.tsx": $$$$$$35,
-    "site/sections/ProductPage/DescriptionMoreInfos.tsx": $$$$$$36,
-    "site/sections/ProductPage/DescriptionNavigator.tsx": $$$$$$37,
-    "site/sections/ProductPage/NutritionalInformation.tsx": $$$$$$38,
+    "site/sections/Product/ProductDetails.tsx": $$$$$$32,
+    "site/sections/Product/ProductShelf.tsx": $$$$$$33,
+    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$34,
+    "site/sections/Product/SearchResult.tsx": $$$$$$35,
+    "site/sections/Product/ShelfWithImage.tsx": $$$$$$36,
+    "site/sections/Product/Wishlist.tsx": $$$$$$37,
+    "site/sections/ProductPage/DescriptionBannerBenefits.tsx": $$$$$$38,
+    "site/sections/ProductPage/DescriptionBannerRight.tsx": $$$$$$39,
+    "site/sections/ProductPage/DescriptionContent.tsx": $$$$$$40,
+    "site/sections/ProductPage/DescriptionFullbanner.tsx": $$$$$$41,
+    "site/sections/ProductPage/DescriptionMoreInfos.tsx": $$$$$$42,
+    "site/sections/ProductPage/DescriptionNavigator.tsx": $$$$$$43,
+    "site/sections/ProductPage/NutritionalInformation.tsx": $$$$$$44,
     "site/sections/Session.tsx": $$$$$$45,
     "site/sections/Social/InstagramPosts.tsx": $$$$$$46,
     "site/sections/Social/WhatsApp.tsx": $$$$$$47,
