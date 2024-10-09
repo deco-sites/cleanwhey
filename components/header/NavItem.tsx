be4f9e7dccd43206796f7017f52a6cebc37cdb04
Diego <diego.o.guimaraes29@gmail.com>
2024-10-09 16:16:28 -0300

    fix: melhorando o click no xr dentro do sup menu

--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -25,84 +25,84 @@ function NavItem({ item }: { item: SiteNavigationElement }) {
         )}
       </a>
 
-      {children && children.length > 0 &&
-        (
-          <div
-            class="max-h-[375px] min-h-[375px] border-b-4 border-orange-300 fixed hidden hover:flex group-hover:flex bg-base-100 z-40 items-center justify-between gap-6 border-t-2 border-b-2 border-base-200 w-screen"
-            style={{
-              top: "0px",
-              left: "0px",
-              marginTop: HEADER_HEIGHT_DESKTOP,
-            }}
-          >
-            <div className="container flex items-center justify-between">
-              <ul class="flex relative flex-col items-start justify-center gap-2 min-w-[200px] border-r-gray-200 border-r pr-5">
-                <li>
-                  <h2 class=" text-lg text-orange-300 font-bold">
-                    {name}
-                  </h2>
+      {children && children.length > 0 && (
+        <div
+          class="max-h-[375px] min-h-[375px] border-b-4 border-orange-300 fixed hidden hover:flex group-hover:flex bg-base-100 z-40 items-center justify-between gap-6 border-t-2 border-b-2 border-base-200 w-screen"
+          style={{
+            top: "0px",
+            left: "0px",
+            marginTop: HEADER_HEIGHT_DESKTOP,
+          }}
+        >
+          <div className="container flex items-center justify-between">
+            <ul class="flex relative flex-col items-start justify-center gap-2 min-w-[200px] border-r-gray-200 border-r pr-5">
+              <li>
+                <h2 class=" text-lg text-orange-300 font-bold">{name}</h2>
+              </li>
+              {children.map((node) => (
+                <li class="p-0 group/lastmenu w-full">
+                  <a
+                    class="group-hover/lastmenu:text-orange-300 text-gray-300  text-base font-bold flex items-center justify-between"
+                    href={node.url}
+                  >
+                    <span>{node.name}</span>
+                    {node.children && node.children.length > 0 && (
+                      <Icon id="arrow-right-custom" class="text-orange-300" />
+                    )}
+                  </a>
+
+                  <ul class=" border-r-gray-200 border-r after:absolute after:right-[90%] after:h-full after:bg-transparent after:w-1/2 min-w-[200px] px-5 h-full group-hover/lastmenu:flex hidden flex-col gap-1 mt-4 absolute top-0 left-full">
+                    {node.children?.map((leaf) => (
+                      <li class="p-0 group/finalmenu w-full">
+                        <a
+                          class="hover:underline group-hover/finalmenu:text-orange-300 text-gray-300 flex items-center justify-between"
+                          href={leaf.url}
+                        >
+                          <span class="text-base text-gray-300 font-bold  uppercase">
+                            {leaf.name}
+                          </span>
+                          {leaf.children && leaf.children.length > 0 && (
+                            <Icon
+                              id="arrow-right-custom"
+                              class="text-orange-300"
+                            />
+                          )}
+                        </a>
+
+                        <ul class=" after:absolute after:right-[90%] after:h-full after:bg-transparent after:w-1/2 min-w-[200px] px-5 h-full group-hover/finalmenu:flex hidden flex-col gap-1 mt-4 absolute top-0 left-full">
+                          {leaf.children?.map((final) => (
+                            <li class="min-w-[200px]">
+                              <a
+                                class="hover:underline min-w-[200px]"
+                                href={final.url}
+                              >
+                                <span class="text-base text-gray-300 min-w-[200px] font-bold  uppercase">
+                                  {final.name}
+                                </span>
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    ))}
+                  </ul>
                 </li>
-                {children.map((node) => (
-                  <li class="p-0 group/lastmenu w-full">
-                    <a
-                      class="group-hover/lastmenu:text-orange-300 text-gray-300  text-base font-bold flex items-center justify-between"
-                      href={node.url}
-                    >
-                      <span>{node.name}</span>
-                      {node.children && node.children.length > 0 && (
-                        <Icon id="arrow-right-custom" class="text-orange-300" />
-                      )}
-                    </a>
+              ))}
+            </ul>
 
-                    <ul class="after:content-[' '] border-r-gray-200 border-r after:absolute after:right-[90%] after:h-full after:bg-transparent after:w-1/2 min-w-[200px] ml-5 h-full group-hover/lastmenu:flex hidden flex-col gap-1 mt-4 absolute top-0 left-full">
-                      {node.children?.map((leaf) => (
-                        <li class="p-0 group/finalmenu w-full">
-                          <a
-                            class="hover:underline group-hover/finalmenu:text-orange-300 text-gray-300 flex items-center justify-between"
-                            href={leaf.url}
-                          >
-                            <span class="text-base text-gray-300 font-bold  uppercase">
-                              {leaf.name}
-                            </span>
-                            {leaf.children && leaf.children.length > 0 && (
-                              <Icon
-                                id="arrow-right-custom"
-                                class="text-orange-300"
-                              />
-                            )}
-                          </a>
-
-                          <ul class="after:content-[' '] after:absolute after:right-[90%] after:h-full after:bg-transparent after:w-1/2 min-w-[200px] ml-5 h-full group-hover/finalmenu:flex hidden flex-col gap-1 mt-4 absolute top-0 left-full">
-                            {leaf.children?.map((final) => (
-                              <li>
-                                <a class="hover:underline" href={final.url}>
-                                  <span class="text-base text-gray-300 font-bold  uppercase">
-                                    {final.name}
-                                  </span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                ))}
-              </ul>
-
-              {image?.url && (
-                <Image
-                  class="p-0"
-                  src={image.url}
-                  alt={image.alternateName}
-                  width={345}
-                  height={189}
-                  loading="lazy"
-                />
-              )}
-            </div>
+            {image?.url && (
+              <Image
+                class="p-0"
+                src={image.url}
+                alt={image.alternateName}
+                width={345}
+                height={189}
+                loading="lazy"
+              />
+            )}
           </div>
-        )}
+        </div>
+      )}
     </li>
   );
 }
