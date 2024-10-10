acced6647ed1cbf7c5b81551b379e79155ec16f7
Martin Adam <martin.viana99@gmail.com>
2024-10-10 14:24:30 -0300

    Feat: Signature Css fix

--- islands/IframeLogin.tsx
+++ islands/IframeLogin.tsx
@@ -1,22 +1,34 @@
+import { useEffect } from "preact/hooks";
+import { cssContent } from "../static/signaturesLogin.ts"; 
+
 interface Props {
   src?: string;
 }
 
-const runOnMount = () => {
+const runOnMount = (customCss: string) => {
   globalThis.onload = () => {
     const iFrame = document.getElementById("proxy-loader") as HTMLIFrameElement;
     if (!iFrame) {
       return console.error("Couldn't find iframe");
     }
     iFrame.height = `${iFrame.contentWindow?.document.body.scrollHeight}`;
+
+    // Adiciona o CSS ao iframe após o carregamento
+    const style = document.createElement("style");
+    style.textContent = customCss;
+    iFrame.contentDocument?.head.appendChild(style); // Insere o CSS no <head> do iframe
   };
 };
 
 export default function ProxyIframe({ src }: Props) {
+  useEffect(() => {
+    runOnMount(cssContent);
+  }, []);
+
   return (
     <>
       <script
-        dangerouslySetInnerHTML={{ __html: `(${runOnMount})();` }}
+        dangerouslySetInnerHTML={{ __html: `(${runOnMount.toString()})("${cssContent}");` }}
       ></script>
       <iframe
         id="proxy-loader"
