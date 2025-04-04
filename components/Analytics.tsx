32778dc39b479caa476ebcb71d2a10db7ae0110b
Gustavo Santos <devgustavosantos@gmail.com>
2025-04-04 10:20:54 -0300

    feat: creates two components for analytics events

---
+++ components/Analytics.tsx
@@ -0,0 +1,75 @@
+import type { AnalyticsEvent, IEvent } from "apps/commerce/types.ts";
+import { scriptAsDataURI } from "apps/utils/dataURI.ts";
+
+interface CustomEvent extends IEvent {
+  name: "sizeSelector";
+}
+
+type EventOnClick = AnalyticsEvent | CustomEvent;
+/**
+ * This function is usefull for sending events on click. Works with both Server and Islands components
+ */
+export const SendEventOnClick = <E extends EventOnClick>({
+  event,
+  id,
+}: {
+  event: E;
+  id: string;
+}) => (
+  <script
+    defer
+    src={scriptAsDataURI(
+      (id: string, event: EventOnClick) => {
+        const elem = document.getElementById(id);
+
+        if (!elem) {
+          return console.warn(
+            `Could not find element ${id}. Click event will not be send. This will cause loss in analytics`
+          );
+        }
+
+        elem.addEventListener("click", () => {
+          globalThis.window.DECO.events.dispatch(event);
+        });
+      },
+      id,
+      event
+    )}
+  />
+);
+
+export const SendEventOnView = <E extends AnalyticsEvent>({
+  event,
+  id,
+}: {
+  event: E;
+  id: string;
+}) => (
+  <script
+    defer
+    src={scriptAsDataURI(
+      (id: string, event: E) => {
+        const elem = document.getElementById(id);
+
+        if (!elem) {
+          return console.warn(
+            `Could not find element ${id}. Click event will not be send. This will cause loss in analytics`
+          );
+        }
+
+        const observer = new IntersectionObserver((items) => {
+          for (const item of items) {
+            if (!item.isIntersecting) continue;
+
+            globalThis.window.DECO.events.dispatch(event);
+            observer.unobserve(elem);
+          }
+        });
+
+        observer.observe(elem);
+      },
+      id,
+      event
+    )}
+  />
+);
